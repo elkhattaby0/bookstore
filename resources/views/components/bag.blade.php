b285302f316bd3b583d8f0561c645c7378e3d456
elkhattaby0 <eelkhattaby@gmail.com>
2024-02-18 16:02:17 +0100

    18-02-24 16:02

--- resources/views/components/bag.blade.php
+++ resources/views/components/bag.blade.php
@@ -1,48 +1,47 @@
 @extends('components.navbar')
 @section('content')
 
-    hello from bag<br/>
+<div class="w-[100%] flex justify-center">
     <table
-        class="border-2 w-[100%] text-center"
+        class="w-[90%] text-center felx justify-center mt-[50px]"
     >
-        <tr>
-            <th>Id</th>
-            <th>Image</th>
-            <th>Name</th>
-            <th>Quntity</th>
-            <th>Price</th>
-            <th>Amount</th>
-            <th>Date</th>
-            <th>Action</th>
+        <tr class="text-[15px] text-gray-700">
+            <th class="w-[5%]">Id</th>
+            <th class="w-[10%] text-left">Image</th>
+            <th class="w-[15%] text-left">Name</th>
+            <th class="w-[10%]">Quntity</th>
+            <th class="w-[10%]">Price</th>
+            <th class="w-[10%]">Amount</th>
+            <th class="w-[10%]">Date</th>
+            <th class="w-[10%]">Action</th>
         </tr>
         @foreach ($bagShowWithDashboards as $i)
-            <tr>
-                <td>{{ $i->value['book_id'] }}</td>
-                <td>
+            <tr class="bg-gray-100 border-2">
+                <td class="w-[5%]">{{ $i->value['book_id'] }}</td>
+                <td class="w-[10%]">
                     <img
                         src="{{ asset('storage/'.$i->dashboard_id->image) }}"
                         alt=""
-                        class="w-[20%]"
+                        class="w-[70px] my-[10px] rounded-md"
                     />
                 </td>
-                <td>{{ $i->dashboard_id->title  }}</td>
-                <td>{{ $i->value['quantity'] }}</td>
-                <td>${{ $i->dashboard_id->price }}</td>
-                <td>${{ $i->dashboard_id->price * $i->value['quantity'] }}</td>
-                <td>{{ $i->value['date'] }}</td>
-                <td>
+                <td class="w-[15%] text-left">{{ $i->dashboard_id->title  }}</td>
+                <td class="w-[10%]">{{ $i->value['quantity'] }}</td>
+                <td class="w-[10%]">${{ $i->dashboard_id->price }}</td>
+                <td class="w-[10%]">${{ $i->dashboard_id->price * $i->value['quantity'] }}</td>
+                <td class="w-[10%]">{{ $i->value['date'] }}</td>
+                <td class="w-[10%]">
                     <form
                         method="POST"
                         action="{{ route('bag_page_delete',$i->value['book_id']) }}"
                     >
                         @csrf
                         @method('DELETE')
-                        <button>Delete</button>
+                        <button class="text-red-600">Delete</button>
                     </form>
                 </td>
             </tr>
         @endforeach
-
     </table>
-
+</div>
 @endsection
